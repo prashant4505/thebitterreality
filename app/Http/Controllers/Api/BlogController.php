c4ad935f25411ebf2ce7a2ecbfee6674cb2548a2
Prashant Singh <prashant.s@svinfotech.net>
2025-02-19 16:32:53 +0530

    Created API for blog create

---
+++ app/Http/Controllers/Api/BlogController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Blog;
+use Illuminate\Support\Facades\Validator;
+
+class BlogController extends Controller
+{
+    public function store(Request $request)
+    {
+        // Validate request data
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        // Return validation errors
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        // Create blog post
+        $blog = new Blog();
+        $blog->title = $request->title;
+        $blog->description = $request->description;
+
+        // Handle image upload if exists
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('images', 'public');
+            $blog->image = $imagePath;
+        }
+
+        $blog->save();
+
+        return response()->json([
+            'message' => 'Blog created successfully!',
+            'blog' => $blog
+        ], 201);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -31,29 +31,29 @@ Route::middleware('guest')->group(function () {
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/register', [RegistrationController::class, 'showRegistrationForm'])->name('register')->middleware('auth');;
+Route::get('/register', [RegistrationController::class, 'showRegistrationForm'])->name('register')->middleware('auth');
 Route::post('/register', [RegistrationController::class, 'register']);
 
-Route::get('/users', [UserController::class, 'index'])->name('users.index')->middleware('auth');;
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy')->middleware('auth');;
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit')->middleware('auth');; // Show edit form
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update')->middleware('auth');; // Handle update request
+Route::get('/users', [UserController::class, 'index'])->name('users.index')->middleware('auth');
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy')->middleware('auth');
+Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit')->middleware('auth'); // Show edit form
+Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update')->middleware('auth'); // Handle update request
 
 Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard')->middleware('auth');
 
-Route::get('/blogs/create', [BlogController::class, 'create'])->name('blogs.create')->middleware('auth');;
-Route::post('/blogs', [BlogController::class, 'store'])->name('blogs.store')->middleware('auth');;
+Route::get('/blogs/create', [BlogController::class, 'create'])->name('blogs.create')->middleware('auth');
+Route::post('/blogs', [BlogController::class, 'store'])->name('blogs.store')->middleware('auth');
 Route::get('/blogs', [BlogController::class, 'index'])->name('blogs.index');
-Route::delete('/blogs/{id}', [BlogController::class, 'destroy'])->name('blogs.destroy')->middleware('auth');;
+Route::delete('/blogs/{id}', [BlogController::class, 'destroy'])->name('blogs.destroy')->middleware('auth');
 Route::get('/blogs/{id}', [BlogController::class, 'show'])->name('blogs.show');
-Route::get('/blogs/{id}/edit', [BlogController::class, 'edit'])->name('blogs.edit')->middleware('auth');;
-Route::put('/blogs/{id}', [BlogController::class, 'update'])->name('blogs.update')->middleware('auth');;
+Route::get('/blogs/{id}/edit', [BlogController::class, 'edit'])->name('blogs.edit')->middleware('auth');
+Route::put('/blogs/{id}', [BlogController::class, 'update'])->name('blogs.update')->middleware('auth');
 
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create')->middleware('auth');;
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store')->middleware('auth');;
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create')->middleware('auth');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store')->middleware('auth');
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
 Route::get('/contact', [ContactController::class, 'showForm'])->name('contact.show');
 Route::post('/contact', [ContactController::class, 'submitForm'])->name('contact.submit');
-Route::get('/contact/messages', [ContactController::class, 'index'])->name('contact.index')->middleware('auth');;
-Route::get('/contact/messages/{id}', [ContactController::class, 'show'])->name('contact.showDetails')->middleware('auth');;
+Route::get('/contact/messages', [ContactController::class, 'index'])->name('contact.index')->middleware('auth');
+Route::get('/contact/messages/{id}', [ContactController::class, 'show'])->name('contact.showDetails')->middleware('auth');
